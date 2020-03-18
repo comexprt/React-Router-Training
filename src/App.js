36e6c675a94a62f9a1fed7e8e2aabba758874dd7
comxprt <johnnoah.ompad@yahoo.com>
2020-03-18 13:24:55 +0800

    play with router using plain routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Route from 'react-router-dom/Route'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const User = () => {
+  return (<h1>Welcome User</h1>)
+}
+
+class App extends Component{
+  render(){
+    return (
+      <Router>
+          <div className="App">
+              <Route path="/" exact  strict render={
+                () => {
+                  return ( 
+                    <h1>Welcome Home</h1>
+                  )
+                }
+              } />
+
+
+              <Route path="/about/" exact strict render={
+                () => {
+                  return ( 
+                    <h1>Welcome About</h1>
+                  )
+                }
+              } />
+
+              <Route path="/user" exact strict component={User} />
+
+
+          </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
